e950f347140b5983c2914596e33532ff17c0c44e
Javier Medina <javiermedinag@hotmail.com>
2026-06-04 15:28:22 -0600

    Comentarios con la explicación

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from flask import Flask
+# Este código es un ejemplo de una aplicación Flask que utiliza GraphQL para crear una API.
 from flask_graphql import GraphQLView
+# Graphene es una biblioteca de Python para crear APIs GraphQL. GraphQLView es una vista de Flask que maneja las solicitudes GraphQL.
 import graphene
-
+# La clase Query define los campos que estarán disponibles en la API GraphQL. En este caso, hay dos campos: "hello" y "Chilpandolfo". Cada campo tiene un resolver que devuelve una cadena de texto personalizada.
 class Query(graphene.ObjectType):
     hello = graphene.String(name=graphene.String(default_value="stranger"))
     Chilpandolfo = graphene.String(name=graphene.String(default_value="Chilpandolfo"))
+    # El resolver de "hello" devuelve un saludo personalizado utilizando el nombre proporcionado. El resolver de "Chilpandolfo" devuelve una cadena que indica que el nombre proporcionado es un gran amigo de Mustafo.
     def resolve_hello(self, info, name):
         return f'Hello {name}!'
+    # El resolver de "Chilpandolfo" devuelve un mensaje personalizado que indica que el nombre proporcionado es un gran amigo de Mustafo.
     def resolve_Chilpandolfo(self, info, name):
         return f'{name}! Es un gran amigo de Mustafo.'
+# Finalmente, se crea un esquema GraphQL utilizando la clase Query y se configura la aplicación Flask para manejar las solicitudes GraphQL en la ruta "/graphql". La aplicación se ejecuta en modo de depuración en el puerto 5000 y está disponible en todas las interfaces de red.
 schema = graphene.Schema(query=Query)
 app = Flask(__name__)   
-app.Eadd_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
+# La función add_url_rule se utiliza para agregar una nueva ruta a la aplicación Flask. En este caso, se agrega la ruta "/graphql" que está asociada con la vista GraphQLView. La vista GraphQLView se configura con el esquema GraphQL que se ha definido y se habilita la interfaz gráfica de GraphiQL para facilitar las pruebas de la API.
+app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
