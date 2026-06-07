df50faff23c2d67023b338e6293a8e7cd8d2e324
Javier Medina <javiermedinag@hotmail.com>
2026-06-06 21:43:44 -0600

    Listado de usuarios y búsqueda

--- main.py
+++ main.py
@@ -8,27 +8,32 @@ class User(graphene.ObjectType):
         id = graphene.ID()
         email = graphene.String()
 
+Usrs = [
+            User(name="Mustafo", id=1, email="mustafo@example.com"),
+            User(name="Chilpandolfo", id=2, email="chilpandolfo@example.com"),
+            User(name="Mustafo Chilpandolfo", id=3, email="mustafochilpandolfo@example.com")
+]
+
 # La clase Query define los campos que estarán disponibles en la API GraphQL. En este caso, hay dos campos: "hello" y "Chilpandolfo". Cada campo tiene un resolver que devuelve una cadena de texto personalizada.
 class Query(graphene.ObjectType):
     hello = graphene.String(name=graphene.String(default_value="stranger"))
     Chilpandolfo = graphene.String(name=graphene.String(default_value="Chilpandolfo"))
-    
+    Usurio = graphene.Field(User, id=graphene.ID(required=True))
     Usuarios = graphene.List(User)
- 
     # El resolver de "hello" devuelve un saludo personalizado utilizando el nombre proporcionado. El resolver de "Chilpandolfo" devuelve una cadena que indica que el nombre proporcionado es un gran amigo de Mustafo.
     def resolve_hello(self, info, name):
         return f'Hello {name}!'
     # El resolver de "Chilpandolfo" devuelve un mensaje personalizado que indica que el nombre proporcionado es un gran amigo de Mustafo.
     def resolve_Chilpandolfo(self, info, name):
         return f'{name}! Es un gran amigo de Mustafo.'
+    def resolve_Usurio(self, info, id):
+        for user in Usrs:
+            if str(user.id) == id:
+                return user
+        return None 
     # El resolver de "Usuarios" devuelve una lista de usuarios predefinidos. Cada usuario tiene un nombre y un ID. Esta lista se define como una variable de clase dentro de
     def resolve_Usuarios(self, info):
-        Usuarios = [
-            User(name="Mustafo", id=1, email="mustafo@example.com"),
-            User(name="Chilpandolfo", id=2, email="chilpandolfo@example.com"),
-            User(name="Mustafo Chilpandolfo", id=3, email="mustafochilpandolfo@example.com")
-        ]
-        return Usuarios
+        return Usrs
 
 # Finalmente, se crea un esquema GraphQL utilizando la clase Query y se configura la aplicación Flask para manejar las solicitudes GraphQL en la ruta "/graphql". La aplicación se ejecuta en modo de depuración en el puerto 5000 y está disponible en todas las interfaces de red.
 schema = graphene.Schema(query=Query)
